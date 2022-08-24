320741d97fb3af3575b91c65e2b2f6e303e1011e
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-23 21:26:22 -0600

    adding in api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,11 @@ const countryEL = document.getElementById('country');
 const weatherForecastEl = document.getElementById('weather-forecast');
 const currentTempEl = document.getElementById('current-temp');
 
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+//Api Key
+const API_KEY = '9f04e9b0a261488afe4fcf44a5e77ce9';
 //secondary function
 setInterval(() => {
     const time = new Date();
@@ -15,8 +20,32 @@ setInterval(() => {
     const hour = time.getHours();
     const hoursIn12HrFormat = hour >= 13 ? hour%12: hour
     const minutes = time.getMinutes();
-    const ampm = hours >= 12 ? 'PM' : 'AM'
+    const ampm = hour >= 12 ? 'PM' : 'AM'
 
 timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + '' + `<span id="am-pm">${ampm}</span>`
 
+dateEL.innerHTML =  days[day] + ', ' + date+ ' ' + months[month]
+
+
 }, 1000);
+
+//calling api
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let {latitude, longitude } = success.coords;
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`).then(res => res.json()).then(data => {
+            console.log(data)
+            showWeatherData(data);
+        })
+
+    })
+}
+function showWeatherData (data){
+    let{humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+    
+
+}
