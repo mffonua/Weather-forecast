ae030cf82ddd10431223f332131b09c405865862
mffonua <108596346+mffonua@users.noreply.github.com>
2022-08-25 16:33:40 -0600

    finished js

--- assets/js/script.js
+++ assets/js/script.js
@@ -65,17 +65,43 @@ function showWeatherData (data){
 
   <div class="weather-item">
     <div>Sunrise</div>
-    <div>${window.Comment(sunrise).format('HH:mm a')}</div> 
+    <div>${window.moment(sunrise * 1000).format('HH:mm a')}</div> 
   </div>
 
   </div> <div class="weather-item">
     <div>Sunset</div>
-    <div>${}</div>
+    <div>${window.moment(sunset*1000).format('HH:mm a')}</div>
   </div>
   
   
   `; //utilizing cdnjs
 
+  let otherDayForcast = ''
+  data.daily.forEach((day, idx) => {
+      if(idx == 0){
+          currentTempEl.innerHTML = `
+          <img src="http://openweathermap.org/img/wn//${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
+          <div class="other">
+              <div class="day">${window.moment(day.dt*1000).format('dddd')}</div>
+              <div class="temp">Night - ${day.temp.night}&#176;C</div>
+              <div class="temp">Day - ${day.temp.day}&#176;C</div>
+          </div>
+          
+          `
+      }else{
+          otherDayForcast += `
+          <div class="weather-forecast-item">
+              <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
+              <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+              <div class="temp">Night - ${day.temp.night}&#176;C</div>
+              <div class="temp">Day - ${day.temp.day}&#176;C</div>
+          </div>
+          
+          `
+      }
+  })
 
 
+  weatherForecastEl.innerHTML = otherDayForcast;
 }
+
